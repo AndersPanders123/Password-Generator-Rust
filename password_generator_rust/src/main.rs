fc04e40f0e2d5bf6bb13045e3dd3e0f390a955aa
AndersPanders123 <Andhoe12@innlandetfylke.no>
2023-11-01 15:07:06 +0100

    -

--- password_generator_rust/src/main.rs
+++ password_generator_rust/src/main.rs
@@ -1,16 +1,15 @@
-use std::char::from_u32;
-use rand::{Rng, thread_rng};
-use rand;
+use rand::Rng;
 
 fn main() {
     let password_length = 15;
     let mut result = String::new();
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     for _ in 0..password_length {
-        let number = thread_rng().gen_range(48..122);
-        let ch = from_u32(number).unwrap();
-        result.push(ch);
+        let random_index = rand::thread_rng().gen_range(0..charset.len());
+        let random_char = charset.chars().nth(random_index).unwrap();
+        result.push(random_char);
     }
-    
+
     println!("{}", result);
 }
